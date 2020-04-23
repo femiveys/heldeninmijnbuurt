32bb35752acdd3d7541c6532caa2db0aeefefb4d
Femi Veys <femi@itsimplyworks.be>
2020-04-23 15:41:22 +0200

    add the Avatar

--- components/ApplicationHeader.tsx
+++ components/ApplicationHeader.tsx
@@ -1,7 +1,12 @@
-import { Button } from "antd";
+import { Button, Avatar } from "antd";
+import { useAuth } from "../base/auth";
 
-export const ApplicationHeader = () => (
-  <div style={{ float: "right" }}>
-    <Button>Logout</Button>
-  </div>
-);
+export const ApplicationHeader = () => {
+  const { firebaseUser } = useAuth();
+  return (
+    <div style={{ float: "right" }}>
+      <Button>Logout</Button>
+      <Avatar shape="square" src={firebaseUser?.photoURL} />
+    </div>
+  );
+};
