e358facce9c34593222d3c213f706792193b0461
Femi Veys <femi@itsimplyworks.be>
2020-04-27 13:35:08 +0200

    make Contact component independant

--- src/components/SearchMouthmask/SearchMouthmask.tsx
+++ src/components/SearchMouthmask/SearchMouthmask.tsx
@@ -1,28 +1,12 @@
 import { ToggleableWidget } from "../ToggleableWidget";
 import { EnterMouthmaskAmount } from "./EnterMouthmaskAmount";
-import { TUser, TRelation } from "../../types";
-import { useEffect } from "react";
 import { SuperHeroContactInfo } from "./SuperHeroContactInfo";
-import { Spin } from "antd";
 import { useTranslation } from "react-i18next";
-import { useUser, useApi } from "../../hooks";
+import { useUser } from "../../hooks";
 
 export const SearchMouthmask = () => {
   const { t } = useTranslation();
   const { user } = useUser();
-  const {
-    isLoading: isFetchingSuperHero,
-    data: superHero,
-    error,
-    callApi: fetchSuperHero,
-  } = useApi<{ user: TUser; relation: TRelation }>(
-    "GET",
-    "requestor/superHero"
-  );
-
-  useEffect(() => {
-    if (user.needsMouthmask) fetchSuperHero();
-  }, []);
 
   return (
     <ToggleableWidget
@@ -30,25 +14,10 @@ export const SearchMouthmask = () => {
       toggleField="needsMouthmask"
       toggleOffConfirmText="Ben je zeker enzovoort?"
     >
-      {user.needsMouthmaskAmount === 0 ? (
+      {user?.needsMouthmaskAmount === 0 ? (
         <EnterMouthmaskAmount />
-      ) : !!superHero ? (
-        <Spin spinning={isFetchingSuperHero}>
-          <SuperHeroContactInfo {...superHero} />
-        </Spin>
       ) : (
-        <div>
-          <p>
-            We hebben jammergenoeg niemand in je buurt gevonden die maskers
-            heeft. We blijven zoeken en zullen je een e-mail sturen zodra we
-            iemand vinden.
-          </p>
-          <p>
-            Laat op sociale media weten van deze applicatie. Misschien vinden we
-            zo wel iemand.
-          </p>
-          <p>Knoppen en zo...</p>
-        </div>
+        <SuperHeroContactInfo />
       )}
     </ToggleableWidget>
   );
--- src/components/SearchMouthmask/SuperHeroContactInfo.tsx
+++ src/components/SearchMouthmask/SuperHeroContactInfo.tsx
@@ -1,24 +1,66 @@
-import { TUser, TRelation } from "../../types";
-import { Card, Space } from "antd";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { Card, Space, Spin, Typography } from "antd";
 
-type TProps = {
-  user: TUser;
-  relation: TRelation;
-};
+import {
+  MailOutlined,
+  UserOutlined,
+  WhatsAppOutlined,
+} from "@ant-design/icons";
+import { TRelationUser } from "../../types";
+import { useApi } from "../../hooks";
 
-export const SuperHeroContactInfo: React.FunctionComponent<TProps> = (
-  props
-) => {
-  return (
-    <Card title="Jouw superheld">
-      <Space size="large" direction="vertical">
-        <div>{props.user.name}</div>
-        <div>
-          <a href={`mailto:${props.user.email}`} target="_blank">
-            {props.user.email}
-          </a>
-        </div>
-      </Space>
-    </Card>
+const { Paragraph } = Typography;
+
+export const SuperHeroContactInfo = () => {
+  const { t } = useTranslation();
+  const {
+    isLoading: isFetchingSuperHero,
+    data: relationUser,
+    error,
+    callApi: fetchSuperHero,
+  } = useApi<TRelationUser>("GET", "requestor/superHero");
+  useEffect(() => {
+    fetchSuperHero();
+  }, []);
+
+  return relationUser?.user ? (
+    <Space>
+      <Card title="Jouw superheld">
+        <Space size="large" direction="vertical">
+          <Space>
+            <UserOutlined style={{ fontSize: 32 }} />
+            {relationUser.user.name}
+          </Space>
+          <Space>
+            <MailOutlined style={{ fontSize: 32 }} />
+            <a href={`mailto:${relationUser.user.email}`} target="_blank">
+              {relationUser.user.email}
+            </a>
+          </Space>
+          {relationUser.user.whatsapp && (
+            <Space>
+              <WhatsAppOutlined style={{ fontSize: 32 }} />
+              {`+32${relationUser.user.whatsapp}`}
+            </Space>
+          )}
+        </Space>
+      </Card>
+      <Paragraph>Some info on how to get in contact ...</Paragraph>
+    </Space>
+  ) : isFetchingSuperHero ? (
+    <Spin tip={t("requestor.contact.loading")} />
+  ) : (
+    <div>
+      <p>
+        We hebben jammergenoeg niemand in je buurt gevonden die maskers heeft.
+        We blijven zoeken en zullen je een e-mail sturen zodra we iemand vinden.
+      </p>
+      <p>
+        Laat op sociale media weten van deze applicatie. Misschien vinden we zo
+        wel iemand.
+      </p>
+      <p>Knoppen en zo...</p>
+    </div>
   );
 };
