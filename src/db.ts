db1a7e7d1b700507e535c25b2d2c26c2e3fff601
Femi Veys <femi@itsimplyworks.be>
2020-05-02 01:32:21 +0200

    db

--- src/db.ts
+++ src/db.ts
@@ -4,8 +4,8 @@ export const db = knex({
   client: "mysql",
   connection: {
     host: "ID179346_mondmaskers.db.webhosting.be",
-    user: "ID179346_mondmaskers",
-    password: "CpJuxxtgy2P49aLu",
-    database: "ID179346_mondmaskers",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_USER,
   },
 });
