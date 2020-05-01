0ff541b4dd1dabbdd6fd88a57d92cbf4cb08ba59
Femi Veys <femi@itsimplyworks.be>
2020-05-01 15:48:36 +0200

    fix the redirects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import EnterStreet from "../components/EnterStreet";
 import Share from "../components/Share";
 import { useUser, useApi } from "../hooks";
 import { grid } from "../helpers";
+import { useEffect } from "react";
+import FullSpinner from "../components/FullSpinner";
 
 const { Paragraph } = Typography;
 
@@ -20,85 +22,91 @@ export default () => {
   const { isLoading, callApi } = useApi("PUT", "me/action");
   const router = useRouter();
 
-  if (user) {
-    if (user.isMaker) router.replace("/superhero");
-    else if (user.needsMouthmask) router.replace("/searching");
-    else if (!user.streetId) router.replace("/new");
-    else {
-      return (
-        <Row>
-          <Col {...grid}>
-            <Result
-              icon={<SmileOutlined />}
-              title="Word een superheld"
-              subTitle={
-                <Typography style={{ textAlign: "left" }}>
-                  <Paragraph>
-                    Iedereen zou een mondmasker moeten kunnen hebben. We hebben
-                    helden nodig en jij kan een superheld worden.
-                  </Paragraph>
-                  <Paragraph>
-                    De superhelden zijn zij die zich aan het naaien zetten en
-                    een aantal mondmaskers maken voor mensen in hun buurt. Dit
-                    moeten er geen honderden zijn. Ook als je maar een aantal
-                    mondmaskers kan naaien, help je veel mensen uit de dood.
-                    Elke mondmasker telt.
-                  </Paragraph>
-                  <Paragraph>
-                    Ook als je geen naaimachine hebt, kan je helpen door over
-                    dit platform te vertellen. Hoe meer mensen weten dat het
-                    bestaat hoe meer superhelden zullen opstaan.
-                  </Paragraph>
-                </Typography>
-              }
-              extra={
-                <div>
-                  <Button
-                    size="large"
-                    type="primary"
-                    style={style}
-                    onClick={async () => {
-                      await callApi({ name: "setIsMaker" });
-                      updateUser({ isMaker: true });
-                    }}
-                  >
-                    Ik maak mondmaskers
-                  </Button>
-                  <Button
-                    style={style}
-                    type="primary"
-                    size="large"
-                    onClick={() => {
-                      Modal.info({
-                        title: "Spread the word...",
-                        icon: <ShareAltOutlined />,
-                        content: <Share />,
-                        centered: true,
-                        maskClosable: true,
-                        okText: t("close"),
-                      });
-                    }}
-                  >
-                    Ik laat anderen weten over dit platform
-                  </Button>
-                  <Button
-                    style={style}
-                    onClick={async () => {
-                      await callApi({ name: "setNeedsMouthmask" });
-                      updateUser({ needsMouthmask: true });
-                    }}
-                  >
-                    Ik zoek een mondmasker
-                  </Button>
-                  <Spin spinning={isLoading} style={style} />
-                </div>
-              }
-            />
-          </Col>
-        </Row>
-      );
+  useEffect(() => {
+    if (user) {
+      if (user.isMaker) router.replace("/superhero");
+      else if (user.needsMouthmask) router.replace("/searching");
+      else if (!user.streetId) router.replace("/new");
     }
-  } else {
-    return <EnterStreet />;
-  }
+  }, [user]);
+
+  // We show a full spinner while redirecting (see above)
+  return user ? (
+    user.isMaker || user.needsMouthmask || !user.streetId ? (
+      <FullSpinner />
+    ) : (
+      <Row>
+        <Col {...grid}>
+          <Result
+            icon={<SmileOutlined />}
+            title="Word een superheld"
+            subTitle={
+              <Typography style={{ textAlign: "left" }}>
+                <Paragraph>
+                  Iedereen zou een mondmasker moeten kunnen hebben. We hebben
+                  helden nodig en jij kan een superheld worden.
+                </Paragraph>
+                <Paragraph>
+                  De superhelden zijn zij die zich aan het naaien zetten en een
+                  aantal mondmaskers maken voor mensen in hun buurt. Dit moeten
+                  er geen honderden zijn. Ook als je maar een aantal mondmaskers
+                  kan naaien, help je veel mensen uit de dood. Elke mondmasker
+                  telt.
+                </Paragraph>
+                <Paragraph>
+                  Ook als je geen naaimachine hebt, kan je helpen door over dit
+                  platform te vertellen. Hoe meer mensen weten dat het bestaat
+                  hoe meer superhelden zullen opstaan.
+                </Paragraph>
+              </Typography>
+            }
+            extra={
+              <div>
+                <Button
+                  size="large"
+                  type="primary"
+                  style={style}
+                  onClick={async () => {
+                    await callApi({ name: "setIsMaker" });
+                    updateUser({ isMaker: true });
+                  }}
+                >
+                  Ik maak mondmaskers
+                </Button>
+                <Button
+                  style={style}
+                  type="primary"
+                  size="large"
+                  onClick={() => {
+                    Modal.info({
+                      title: "Spread the word...",
+                      icon: <ShareAltOutlined />,
+                      content: <Share />,
+                      centered: true,
+                      maskClosable: true,
+                      okText: t("close"),
+                    });
+                  }}
+                >
+                  Ik laat anderen weten over dit platform
+                </Button>
+                <Button
+                  style={style}
+                  onClick={async () => {
+                    await callApi({ name: "setNeedsMouthmask" });
+                    updateUser({ needsMouthmask: true });
+                  }}
+                >
+                  Ik zoek een mondmasker
+                </Button>
+                <Spin spinning={isLoading} style={style} />
+              </div>
+            }
+          />
+        </Col>
+      </Row>
+    )
+  ) : (
+    <EnterStreet />
+  );
 };
