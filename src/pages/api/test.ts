f58d7969b905957cf9117ce766f319b7f845ff2d
Femi Veys <femi@itsimplyworks.be>
2020-05-12 17:40:51 +0200

    DKIM

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -6,11 +6,13 @@ import { sendMail } from "../../apiHelpers/mailer";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     try {
-      const result = await sendMail("femiveys@gmail.com", "accepted", {
-        name: "Bob",
-        email: "email",
-        whatsapp: "488014092",
-      });
+      const result = await sendMail(
+        "femiveys@gmail.com,carolinerosu@hotmail.com",
+        "message",
+        // @ts-ignore
+        undefined,
+        "Is deze mail correct aangekomen?"
+      );
       console.log(result);
       res.send(result);
     } catch (error) {
